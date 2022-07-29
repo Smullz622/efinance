f76d0b744b4e6a61d839b01ffecf7023c883e8f1
rlatjdwls0325 <rlatjdwls0325@gmail.com>
2022-07-29 16:57:36 +0900

    added LoanController class

---
+++ src/main/java/com/example/ist412group4/controller/LoanController.java
@@ -0,0 +1,42 @@
+package com.example.ist412group4.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+public class LoanController {
+
+    @GetMapping("/saveLoan")
+    public String saveLoan(@ModelAttribute("loan") Loan loan)
+    {
+        loanService.saveLoan(loan);
+        return "redirect:/";
+    }
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
+        int pageSize = 5;
+
+        Page<Loan> page = loanService.findPaginated(pageNo, pageSize, sortField, sortDir);
+        List<Loan> listLoans = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPage", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("listloans", listLoans);
+        return "index";
+    }
+}
